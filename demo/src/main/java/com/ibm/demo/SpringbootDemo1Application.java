b80bf1d4c497c0e0951f7f70fbe58b80f6824d60
unknown <ibmjavanh17@iiht.tech>
2021-03-31 09:44:34 +0530

    added transaction

--- demo/src/main/java/com/ibm/demo/SpringbootDemo1Application.java
+++ demo/src/main/java/com/ibm/demo/SpringbootDemo1Application.java
@@ -3,9 +3,13 @@ package com.ibm.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringbootDemo1Application {
 
 	public static void main(String[] args) {
@@ -15,5 +19,9 @@ public class SpringbootDemo1Application {
 	@Bean
 	RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+	
+	PlatformTransactionManager transactionManager(){
+		return new MongoTransactionManager();
 	}
 }
--- demo/src/main/java/com/ibm/demo/service/OrderService.java
+++ demo/src/main/java/com/ibm/demo/service/OrderService.java
@@ -2,6 +2,7 @@ package com.ibm.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import com.ibm.demo.entity.Order;
@@ -14,13 +15,16 @@ public class OrderService { // spring bean
 	@Autowired
 	RestTemplate getTaxesTemplate;
 
+	@Transactional
 	public String createOrder(Order order) {
 		// call getTaxes
 		Float tax = getTaxesTemplate.getForObject("http://localhost:8080/getTaxes?price={price}", Float.class,
 				order.getPrice());
 		System.out.println(tax);
-		order.setTax(tax); //saving tax in order class
+		order.setTax(tax); // saving tax in order class
 		Order savedOrder = orderRepository.save(order);
+//		if(order != null)
+//			throw new RuntimeException();
 		return savedOrder.getId();
 	}
 
